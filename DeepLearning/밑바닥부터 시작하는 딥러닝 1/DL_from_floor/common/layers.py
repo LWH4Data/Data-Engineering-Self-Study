935682bb90a4184164b43e5459518c9bc41ff1a0
이웅희 <lwh4sty@gmail.com>
2026-09-04 17:52:34 +0900

    밑바닥 딥러닝 중간 저장

--- DeepLearning/밑바닥부터 시작하는 딥러닝 1/DL_from_floor/common/layers.py
+++ DeepLearning/밑바닥부터 시작하는 딥러닝 1/DL_from_floor/common/layers.py
@@ -1,4 +1,6 @@
 import numpy as np
+from functions import softmax
+from functions import cross_entropy_error
 
 class Relu:
     def __init__(self):
@@ -34,5 +36,54 @@ class Sigmoid:
 
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        # dX = dout · Wᵀ
+        dx = np.dot(dout, self.W.T)
+
+        # dW = Xᵀ · dout
+        self.dW = np.dot(self.x.T, dout)
+
+        # 덧셈의 미분은 1이므로 dout이 그대로 전달된다.
+        # 같은 편향을 모든 데이터가 공유하므로 배치 방향으로 합산한다.
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None  # 손실함수
+        self.y = None  # softmax의 출력
+        self.t = None  # 정답 레이블(원-핫 벡터)
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        
+        # Softmax와 교차 엔트로피 손실을 결합해서 미분하면
+        # 입력 점수 x에 대한 기울기는 (y - t) / batch_size가 된다.
+        dx = (self.y - self.t) / batch_size
 
         return dx
